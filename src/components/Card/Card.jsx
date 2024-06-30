ac187fe657614c3caf9e9991ca0773cac54061af
fukuniji <marusina.nat@gmail.com>
2024-07-01 01:40:12 +0500

    feat: add showing translation in card using useState

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,26 @@
 import style from "./Card.module.scss";
+import { useState } from "react";
 
 export default function Card({ data }) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
   return (
     <>
       <div className={style.card}>
         <h3>{data[0].english}</h3>
         <p>{data[0].transcription}</p>
-        <div>Посмотреть</div>
-        {/* <p>{data[0].russian}</p> */}
+        {isFlipped ? (
+          <p className={style.translation}>{data[0].russian}</p>
+        ) : (
+          <button
+            onClick={() => {
+              setIsFlipped(true);
+            }}
+            className={`${style.button_flip} ${style.button}`}
+          >
+            Посмотреть
+          </button>
+        )}
       </div>
     </>
   );
