56268226310236baa731ce3229f965e2d4f3bd4f
fukuniji <marusina.nat@gmail.com>
2024-07-28 18:38:20 +0500

    feat: add styles to NavLink in header (incl. active)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,41 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 import style from "./Header.module.scss";
 
 export default function Header() {
   return (
     <header className={style.container}>
-      <NavLink to="/">
+      <Link to="/">
         <div className={style.logo}>
-          <img className={style.logo_img} src={logo} alt="logo" title="Verba" />
+          <img className={style.logo_img} src={logo} alt="logo" />
         </div>
-      </NavLink>
+      </Link>
 
       <h2>Verba</h2>
 
       <nav className={style.menu}>
-        <NavLink className={style.navlink} to="/">
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? `${style.navlink} ${style.active}` : style.navlink
+          }
+          to="/"
+        >
           На главную
         </NavLink>
-        <NavLink className={style.navlink} to="/cards">
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? `${style.navlink} ${style.active}` : style.navlink
+          }
+          to="/cards"
+        >
           Карточки
         </NavLink>
-        <NavLink className={style.navlink} to="/words">
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? `${style.navlink} ${style.active}` : style.navlink
+          }
+          to="/words"
+        >
           Список слов
         </NavLink>
       </nav>
